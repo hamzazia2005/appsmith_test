fb091359cbb4043ac25b27df053d8cacf602dd81
Hamza Zia <hamzazia2005@gmail.com>
2024-08-14 20:23:10 +0000

    add checks for modal

--- pages/Movies list/jsobjects/utils/utils.js
+++ pages/Movies list/jsobjects/utils/utils.js
@@ -11,6 +11,43 @@ export default {
 		}
 	},
 	
+	updateModalValues () {
+		if(
+		!Input1.text ||
+		!Input2.text ||
+		!DatePicker1.formattedDate ||
+		!Input3.text ||
+		!Input4.text ||
+		!Input5.text ||
+		!Input6.text ||
+		!Input7.text ||
+		!Input8.text ||
+		!Input9.text ||
+		!Input10.text ||
+		!Input11.text ||
+		!Input12.text ||
+		!Input13.text ||
+		!Input14.text ||
+		!Input15.text ||
+		!Input16.text ||
+		!Input17.text ||
+		!Input18.text ||
+		!Input19.text ||
+		!Input20.text 
+		) {
+			return showAlert("All fields are requid", 'error')
+		}
+		
+		{{updateQueryModal.run().then(() => {
+			getMovies.run();
+			closeModal('');
+			showAlert('Movie details updated', 'success');
+		}).catch(() => {
+			showAlert('Something went wrong', 'error');
+		});}}
+		
+	},
+	
 	setValuesInModal () {
 		Input1.setValue(moviesTable.selectedRow.title)
 		Input2.setValue(moviesTable.selectedRow.imdb_id)
