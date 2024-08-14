8f3c3d7753666fc49da3e2d8ae34ee41df8c123e
Hamza Zia <hamzazia2005@gmail.com>
2024-08-14 20:11:27 +0000

    make all fields required in add movie form

--- pages/Add Movies/jsobjects/JSObject1/JSObject1.js
+++ pages/Add Movies/jsobjects/JSObject1/JSObject1.js
@@ -1,5 +1,31 @@
 export default {
 	async saveMovie () {
-		return addMovie.run().then(() => showAlert(('Movie details added', 'success'))).then(() => resetWidget('addMovieForm'))
+		if(
+			!title.text || 
+			!voteAverage.text || 
+			!voteCount.text || 
+			!revenue.text || 
+			!releaseDate.formattedDate || 
+			!originalTitle.text || 
+			!budget.text || 
+			!homepage.text || 
+			!overview.text || 
+			!popularity.text || 
+			!productionCompanies.text || 
+			!spokenLang.text || 
+			!tagline.text || 
+			!productionCountries.text || 
+			!posterPath.text || 
+			!backdropPath.text || 
+			!keywords.text || 
+			!imdbId.text || 
+			!status.text
+		) {
+			return showAlert('All Fields are required', 'error')
+		}
+		return addMovie.run().then(() => {
+			showAlert('Movie details added', 'success')
+			navigateTo('Movie List')
+		}).then(() => resetWidget('addMovieForm'))
 	}
 }
